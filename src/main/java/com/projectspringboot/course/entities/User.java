18ae47a614c3ed9c86b3f0717a932b262235d54a
Bryan Costa <bryancmoraes776@gmail.com>
2023-10-31 21:52:54 -0300

    H2 database, test profile, JPA

--- src/main/java/com/projectspringboot/course/entities/User.java
+++ src/main/java/com/projectspringboot/course/entities/User.java
@@ -2,10 +2,21 @@ package com.projectspringboot.course.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 //Serializable é usado para transformar os objetos em cadeias de bytes
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	//chave primária da tabela do banco de dados
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
 	private Long id;
 	private String name;
 	private String email;
