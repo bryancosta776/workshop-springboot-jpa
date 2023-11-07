4e3246151b39ba507149743a78107945c40f2ec1
Bryan Costa <bryancmoraes776@gmail.com>
2023-11-06 22:30:04 -0300

    order, instant, ISO 8601 part 2

--- src/main/java/com/projectspringboot/course/entities/Order.java
+++ src/main/java/com/projectspringboot/course/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,13 +18,15 @@ import jakarta.persistence.Table;
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	
-	//chave primária da tabela do banco de dados
+	
 	
 	private static final long serialVersionUID = 1L;
-	
+	//chave primária da tabela do banco de dados
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
 	//Relação de muitos para um
